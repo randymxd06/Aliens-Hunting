268dd9d2d5349456134d4620165b2aee4e6201da
randymxd06 <randym0624@gmail.com>
2023-03-11 23:43:58 -0400

    Este es el juego de realidad aumentada para celulares

---
+++ Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    //for this to work both need colliders, one must have rigid body (spaceship) the other must have is trigger checked.
+    void OnTriggerEnter (Collider col)
+    {
+
+        Destroy(col.gameObject);
+
+        if (GameObject.FindGameObjectsWithTag("Player").Length == 0){
+            GameObject enemy = Instantiate(Resources.Load("enemy", typeof(GameObject))) as GameObject;
+            GameObject enemy1 = Instantiate(Resources.Load("enemy1", typeof(GameObject))) as GameObject;
+            GameObject enemy2 = Instantiate(Resources.Load("enemy2", typeof(GameObject))) as GameObject;
+            GameObject enemy3 = Instantiate(Resources.Load("enemy3", typeof(GameObject))) as GameObject;
+        }
+
+        Destroy(gameObject);
+
+    }
+
+}
